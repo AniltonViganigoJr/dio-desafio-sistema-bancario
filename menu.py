8bb0c07c5354c57c719ca352897a48c17ced9421
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2025-11-07 00:36:18 -0300

    feat: adiciona novas opções no menu do sistema bancário

--- menu.py
+++ menu.py
@@ -4,6 +4,9 @@ def apresentar_menu() -> str:
      return textwrap.dedent("""     
         [d] Depositar
         [s] Sacar
+        [c] Cadastrar Cliente
+        [n] Cadastrar Nova Conta
         [e] Extrato
+        [l] Listar Clientes
         [q] Sair
         => """)
